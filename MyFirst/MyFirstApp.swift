e82be6ff2114e2730a566630a8d214e337e9f10a
jasonajordan <jasonajordan0@gmail.com>
2024-09-10 10:03:12 -0400

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Jason Jordan on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
